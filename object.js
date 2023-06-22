11ceb98642ef5d33f563fe3537589d89a90f67a9
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-06-22 23:04:34 +0800

    Done method chaining.

--- object.js
+++ object.js
@@ -4,18 +4,27 @@ class User {
         // properties
         this.email = email;
         this.name = name;
+        this.score = 0;
     }
     // methods
     login() {
         console.log(this.email, "just logged in");
+        return this;
     }
 
     logout() {
         console.log(this.email, "just logged out");
+        return this;
+    }
+
+    updateScore() {
+        this.score++;
+        console.log(this.email, "score is now", this.score);
+        return this;
     }
 }
 
 var userOne = new User('cris@gmail.com', 'Cris-aian Vergara');
 var userTwo = new User('ben@gmail.com', 'Ben Ten');
 
-userOne.login();
+userOne.login().updateScore().updateScore().logout();
