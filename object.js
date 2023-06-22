3d105e4db910ae4cf6edea8d143698b08259d2b9
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-06-22 22:57:38 +0800

    Done class methods.

--- object.js
+++ object.js
@@ -1,15 +1,21 @@
 class User {
+    // constructor
     constructor(email, name) {
+        // properties
         this.email = email;
         this.name = name;
+    }
+    // methods
+    login() {
+        console.log(this.email, "just logged in");
+    }
+
+    logout() {
+        console.log(this.email, "just logged out");
     }
 }
 
 var userOne = new User('cris@gmail.com', 'Cris-aian Vergara');
 var userTwo = new User('ben@gmail.com', 'Ben Ten');
 
-console.log(userOne.name);
-console.log(userOne.email);
-
-console.log(userTwo.name);
-console.log(userTwo.email);
+userOne.login();
