50fb69fcda2782d35331466fee6bd8a0ae6800b1
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-06-23 09:14:14 +0800

    Done constructors (under the hood.)

--- object.js
+++ object.js
@@ -1,43 +1,15 @@
-class User {
-    // constructor
-    constructor(email, name) {
-        // properties
-        this.email = email;
-        this.name = name;
-        this.score = 0;
-    }
-    // methods
-    login() {
-        console.log(this.email, "just logged in");
-        return this;
-    }
-
-    logout() {
-        console.log(this.email, "just logged out");
-        return this;
-    }
-
-    updateScore() {
-        this.score++;
-        console.log(this.email, "score is now", this.score);
-        return this;
+function User(email, name) {
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function() {
+        console.log(this.email +' has logged in');
     }
 }
 
-class Admin extends User {
-    deleteUser(user) {
-        users = users.filter(u => {
-            return u.email != user.email;
-        })
-    }
-}
 
 var userOne = new User('cris@gmail.com', 'Cris-aian Vergara');
 var userTwo = new User('ben@gmail.com', 'Ben Ten');
-var admin = new Admin('anna@gmail.com', "Anna Smith");
 
-var users = [userOne, userTwo, admin];
-
-admin.deleteUser(userOne);
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
